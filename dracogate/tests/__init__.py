60ef7940ca0bd645e76f7fa71a8a987aaadefe00
gchang12 <gc8@hawaii.edu>
2025-07-24 05:45:48 -0700

    Revert "Deleted all custom modules"
    This reverts commit 74d1a7dee2196d548e4ca359fe0969ee91b87d0a.

---
+++ dracogate/tests/__init__.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from .models import MorphTests
+
+# Create your tests here.
+
+class AenirTests(TestCase):
+    """
+    """
+
+    def setUp(self):
+        """
+        """
