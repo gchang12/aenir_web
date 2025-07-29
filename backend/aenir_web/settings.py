c00208121dccb614d0002ca9eca4df0faf4aed17
gchang12 <gc8@hawaii.edu>
2025-07-28 19:58:42 -0700

    ???

--- backend/aenir_web/settings.py
+++ backend/aenir_web/settings.py
@@ -120,3 +120,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
