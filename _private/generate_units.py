d35231141b6b063f63980687b0953685820c29c7
gchang12 <gc8@hawaii.edu>
2026-01-30 21:08:23 -1000

    Added function to generate unit-list

---
+++ _private/generate_units.py
@@ -0,0 +1,50 @@
+"""
+"""
+
+import json
+
+from aenir.morph import (
+    Morph4,
+    Morph5,
+    Morph6,
+    Morph7,
+    Morph8,
+    Morph9,
+)
+
+MORPHS = {
+    4: Morph4,
+    5: Morph5,
+    6: Morph6,
+    7: Morph7,
+    8: Morph8,
+    9: Morph9,
+}
+
+KWARGS_SET = {
+    4: {"father": "Arden"},
+    5: {},
+    6: {"hard_mode": False, "number_of_declines": 0, "route": "Lalum"},
+    7: {"lyn_mode": False, "hard_mode": False},
+    8: {},
+    9: {},
+}
+
+UNITS_BY_GAME = []
+
+for i in range(4, 9):
+    morph_cls = MORPHS[i]
+    kwargs = KWARGS_SET[i]
+    unit_list = []
+    for name in morph_cls.get_true_character_list():
+        morph = morph_cls(name, **kwargs)
+        unit = {
+            "name": name,
+            "class": morph.current_cls,
+            "lv": morph.current_lv,
+        }
+        unit_list.append(unit)
+    UNITS_BY_GAME.append([i, unit_list])
+
+with open("UNITS_BY_GAME.json", mode="w") as wfile:
+    json.dump(UNITS_BY_GAME, wfile, indent=2)
