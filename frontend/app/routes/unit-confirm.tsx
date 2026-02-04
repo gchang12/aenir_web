245cc6e5ef75f4e405edfdb950ddf388a94299ab
gchang12 <gc8@hawaii.edu>
2026-02-04 08:30:34 -1000

    Having trouble getting React to recognize stats.map as a function

--- frontend/app/routes/unit-confirm.tsx
+++ frontend/app/routes/unit-confirm.tsx
@@ -6,18 +6,43 @@ import type { Route } from "./+types/unit-confirm";
 
 import type {
   GameID,
+  Unit,
+  Game,
+  Stats,
 } from "../lib/_types";
+
+import {
+  UNITS,
+} from "../lib/UNITS";
+
+import {
+  GAMES,
+} from "../lib/GAMES";
 
 import {
   StatTable,
 } from "../lib/StatTables";
 
 export default function({ params }: Route.ClientLoaderArgs) {
-  const { gameId, unitName } : {gameId: GameID, unit: string} = params;
+  const { gameId, unitName } : {gameId: GameID, unitName: string} = params;
+  const [unit]: [Unit] = UNITS.filter(someUnit => someUnit.name === unitName && "fe" + someUnit.gameNo === gameId);
+  const [game]: [Game] = GAMES.filter(someGame => "fe" + someGame.no === gameId);
+  const stats: Stats = [
+    ["HP", 18],
+    ["Pow", 5],
+    ["Skl", 5],
+    ["Spd", 7],
+    ["Lck", 7],
+    ["Def", 5],
+    ["Res", 0],
+    ["Con", 6],
+    ["Mov", 5],
+  ];
   return (
     <>
-    <h1>{unitName}</h1>
-    <h2>{gameId}</h2>
+    <h1>{game.title}</h1>
+    <h2>{unitName}</h2>
+    <StatTable unit={unit} stats={stats} />
     </>
   );
 };
