ad28201ff9122555947298fd9c9dbcb03603c336
gchang12 <gc8@hawaii.edu>
2026-02-08 01:41:19 -1000

    I declared two new models for data-storage purposes.

--- backend/dracogate/models.py
+++ backend/dracogate/models.py
@@ -1,3 +1,24 @@
+"""
+"""
+
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+# User
+# ArenaTeam
+# Morph
+
+class Deadlords(models.Model):
+    """
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Morph(models.Model):
+    """
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    deadlords = models.ManyToManyField(to=Deadlords)
