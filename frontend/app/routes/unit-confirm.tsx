7976381329808830cb616028ee2750c54a1ec3cc
gchang12 <gc8@hawaii.edu>
2026-02-08 17:28:30 -1000

    I fixed the stat-reload problem by removing state.
    How did this fix the problem? This warrants investigation.

--- frontend/app/routes/unit-confirm.tsx
+++ frontend/app/routes/unit-confirm.tsx
@@ -29,7 +29,7 @@ import {
   previewMorph,
 } from "../lib/quintessence";
 
-export async function clientLoader({ params }: Route.ClientLoaderArgs) : { game: Game; unit: Unit; defaultMorph: Morph } {
+export async function loader({ params }: Route.LoaderArgs) : { game: Game; unit: Unit; defaultMorph: Morph } {
   const { gameId, unitName } : {gameId: GameID, unitName: string} = params;
   const unit: Unit = UNITS.find(someUnit => someUnit.name === unitName && "fe" + someUnit.gameNo === gameId);
   const game: Game = GAMES.find(someGame => "fe" + someGame.no === gameId);
@@ -37,12 +37,12 @@ export async function clientLoader({ params }: Route.ClientLoaderArgs) : { game:
   return { game, unit, defaultMorph };
 };
 
-export default async function({
+export default function UnitConfirm({
   loaderData,
 }: Route.ComponentProps) : React.ReactElement {
   const { game, unit, defaultMorph } = loaderData;
-  const [morph, setMorph] = React.useState<Morph>(defaultMorph);
-  const { currentStats, maxStats } = morph;
+  // const [morph, setMorph] = React.useState<Morph>(defaultMorph);
+  const { currentStats, maxStats } = defaultMorph;
   return (
     <div id="unit-confirm">
       <h1>{game.title}</h1>
