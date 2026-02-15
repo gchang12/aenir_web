d35daa0b33b74c05eec2d533e3d210bf12cc5869
gchang12 <gc8@hawaii.edu>
2026-02-14 17:06:46 -1000

    I updated my backend models.

--- backend/dracogate/models.py
+++ backend/dracogate/models.py
@@ -1,35 +1,127 @@
 """
 """
 
+import json
+
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
-# Create your models here.
-
-# User
-# ArenaTeam
-# Morph
-
-class Deadlords(models.Model):
+def validate_game_no(game_no):
     """
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    if game_no not in range(4, 10):
+        raise ValidationError(
+            "No support for FE%(game_no)d yet.",
+            params={"game_no": game_no},
+        )
+
+def validate_name(name):
+    """
+    """
+    pass
+
+def validate_options(options):
+    """
+    """
+    valid_options = (
+        "father",
+        "hard_mode",
+        "number_of_declines",
+        "route",
+        "lyn_mode",
+    )
+    invalid_options = sorted(lambda item: tuple(options).index(item), set(options) - set(valid_options))
+    if invalid_options:
+        raise ValidationError(
+            _("Invalid options were found: %(invalid_options)s."),
+            params={"invalid_options": invalid_options},
+        )
+    for option in set.intersection(valid_options, options):
+        if option == "father" and options[option] not in (
+            'Arden',
+            'Azel',
+            'Alec',
+            'Claude',
+            'Jamka',
+            'Dew',
+            'Noish',
+            'Fin',
+            'Beowolf',
+            'Holyn',
+            'Midayle',
+            'Levin',
+            'Lex',
+       ):
+            raise ValidationError(
+                _("Invalid 'father' found: '%(father)s'."),
+                params={"father": options[option]},
+            )
+        if option in ("hard_mode", "lyn_mode") and options[option] not in ('true', 'false'):
+            raise ValidationError(
+                _("Non-pseudo-boolean value found for '%(mode)s': '%(nonboolean)s'."),
+                params={"mode": option, "nonboolean": options[option]},
+            )
+        if option == "number_of_declines" and options[option] not in (str(i) for i in range(0, 4)):
+            raise ValidationError(
+                _("Invalid 'number_of_declines' value found: '%(number_of_declines)s.'"),
+                params={"number_of_declines": options[option]},
+            )
+        if route == "route" and options[option] not in ("Elphin", "Lalum"):
+            raise ValidationError(
+                _("Invalid 'route' value found: %(route)s."),
+                params={"route": options[option]},
+            )
+
+def validate_history(history):
+    """
+    """
+    # check that it's a list of [str, [...args]] pairs
+    # also, validate each entry.
+    pass
 
 class Morph(models.Model):
     """
     """
-    id = models.CharField(primary_key=True)
+    # meta: composite pk
+    morph_id = models.CharField(
+        max_length=21,
+    )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         null=True,
     )
-    deadlords = models.ManyToManyField(
-        to=Deadlords,
+    # content
+    game_no = models.PositiveSmallIntegerField(
+        validators=[validate_game_no],
     )
-    history = models.JSONField()
-    #game_no = models.PositiveSmallIntegerField()
-    #unit_name = models.PositiveSmallIntegerField()
-    #init_kwargs = models.JSONField()
+    name = models.CharField(
+        max_length=9,
+        validators=[validate_name],
+    )
+    options = models.JSONField(
+        validators=[validate_options],
+        default=dict,
+    )
+    history = models.JSONField(
+        default=list,
+        validators=[validate_history],
+    )
+
+    class Meta:
+        unique_together = [
+            ["user", "morph_id"],
+        ]
+
+class Deadlords(models.Model):
+    """
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    deadlords = models.ManyToManyField(
+        to=Morph,
+    )
+
