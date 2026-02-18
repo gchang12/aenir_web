7ab1aaf30cf938bfcb8cc22514467a456eb9a132
gchang12 <gc8@hawaii.edu>
2026-02-17 15:24:53 -1000

    I wrote some test stubs.

--- frontend/src/tests/routes.test.tsx
+++ frontend/src/tests/routes.test.tsx
@@ -1,3 +1,56 @@
+import {
+  render,
+  screen,
+} from "@testing-library/react";
+import {
+  expect,
+  test,
+  describe,
+  it,
+  beforeAll,
+  afterEach,
+  afterAll,
+} from 'vitest';
+
+describe("GameSelect", () => {
+
+  it("lists all game-ID's.", () => {
+  });
+
+  it("lists game titles.", () => {
+  });
+
+  it("renders images of each game.", () => {
+  });
+
+});
+
+describe("UnitSelect", () => {
+
+  it("shows the images of all units.", () => {
+  });
+
+  it("shows the names of all units.", () => {
+  });
+
+});
+
+describe("UnitConfirm", () => {
+
+  it("shows the image of the selected unit.", () => {
+  });
+
+  it("shows the class and level of the selected unit.", () => {
+  });
+
+  it("shows the stats of the selected unit.", () => {
+  });
+
+  it("allows the user to toggle the initialization options (father, hard_mode, lyn_mode, route, number_of_declines), the stats changing in accordance with the selection.", () => {
+  });
+
+});
+
 /* TODO
 Root
 GameSelect:
