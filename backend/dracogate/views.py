9d44eb4fec70f22fa6a30e3c1de04659cd253fd1
gchang12 <gc8@hawaii.edu>
2026-02-20 16:48:13 -1000

    I declared some methods for interacting with the morph.

--- backend/dracogate/views.py
+++ backend/dracogate/views.py
@@ -18,42 +18,6 @@ class MorphViewSet(viewsets.ViewSet):
     """
     morphs = {}
 
-    @staticmethod
-    def bundle_stats(morph):
-        """
-        """
-        stats = []
-        current_stats = morph.current_stats.as_dict()
-        max_stats = morph.max_stats.as_dict()
-        absmax_stats = morph.Stats.ABSOLUTE_MAXES()
-        for indexno, stat in enumerate(morph.Stats.STAT_LIST()):
-            stats.append((stat, current_stats[stat] / 100, max_stats[stat] / 100, absmax_stats[indexno]))
-        return stats
-
-    @staticmethod
-    def parse_args(dictlike):
-        """
-        """
-        game_no = int(dictlike.get("game_no"))
-        name = dictlike.get("name")
-        morph_options = (
-            "father",
-            "hard_mode",
-            "number_of_declines",
-            "route",
-            "lyn_mode",
-        )
-        kwargs = {key: dictlike.get(key) for key in morph_options if dictlike.get(key) is not None}
-        for kwarg, kwval in kwargs.items():
-            if kwarg in ("hard_mode", "lyn_mode"):
-                kwargs[kwarg] = {
-                    "true": True,
-                    "false": False,
-                }[kwval]
-            if kwarg == "number_of_declines":
-                kwargs[kwarg] = int(kwval)
-        return (game_no, name, kwargs)
-
     def create(self, request):
         """
         """
@@ -100,7 +64,114 @@ class MorphViewSet(viewsets.ViewSet):
             data = {"error": "UNIT_DNE"}
         return Response(data)
 
-    # retrieve
-    # update
-    # partial_update
-    # destroy
+    def partial_update(self, request):
+        """
+        For previewing.
+        """
+
+    def update(self, request):
+        """
+        For changing server-side data.
+        """
+
+    def destroy(self, request):
+        """
+        For deleting morph objects from the viewset.
+        """
+        morph_id = request.data.get("morph_id")
+        status = {"success": None}
+        try:
+            self.morphs.pop(morph_id)
+            status['success'] = True
+        except KeyError as err:
+            status['success'] = False
+        return Response(status)
+
+    #level_up, promote, use_stat_booster, use_growths_item, equip_band, unequip_band, equip_scroll, unequip_scroll
+
+    @staticmethod
+    def level_up(morph, **kwargs):
+        """
+        """
+
+    @staticmethod
+    def promote(morph, **kwargs):
+        """
+        """
+
+    # FE5,6,7,8,9
+    @staticmethod
+    def use_stat_booster(morph, **kwargs):
+        """
+        """
+
+    # FE7
+    @staticmethod
+    def use_afas_drops(morph, **kwargs):
+        """
+        """
+
+    # FE8
+    @staticmethod
+    def use_metiss_tome(morph, **kwargs):
+        """
+        """
+
+    # FE9
+    @staticmethod
+    def equip_band(morph, **kwargs):
+        """
+        """
+
+    @staticmethod
+    def unequip_band(morph, **kwargs):
+        """
+        """
+
+    # FE5
+    @staticmethod
+    def equip_scroll(morph, **kwargs):
+        """
+        """
+
+    @staticmethod
+    def unequip_scroll(morph, **kwargs):
+        """
+        """
+
+    @staticmethod
+    def bundle_stats(morph):
+        """
+        """
+        stats = []
+        current_stats = morph.current_stats.as_dict()
+        max_stats = morph.max_stats.as_dict()
+        absmax_stats = morph.Stats.ABSOLUTE_MAXES()
+        for indexno, stat in enumerate(morph.Stats.STAT_LIST()):
+            stats.append((stat, current_stats[stat] / 100, max_stats[stat] / 100, absmax_stats[indexno]))
+        return stats
+
+    @staticmethod
+    def parse_args(dictlike):
+        """
+        """
+        game_no = int(dictlike.get("game_no"))
+        name = dictlike.get("name")
+        morph_options = (
+            "father",
+            "hard_mode",
+            "number_of_declines",
+            "route",
+            "lyn_mode",
+        )
+        kwargs = {key: dictlike.get(key) for key in morph_options if dictlike.get(key) is not None}
+        for kwarg, kwval in kwargs.items():
+            if kwarg in ("hard_mode", "lyn_mode"):
+                kwargs[kwarg] = {
+                    "true": True,
+                    "false": False,
+                }[kwval]
+            if kwarg == "number_of_declines":
+                kwargs[kwarg] = int(kwval)
+        return (game_no, name, kwargs)
+
