190d257dfe26281bbc5a289606ae9c4bb6d7209c
gchang12 <gc8@hawaii.edu>
2026-02-20 16:37:45 -1000

    I attached some helper methods to the class viewset.

--- backend/dracogate/views.py
+++ backend/dracogate/views.py
@@ -12,45 +12,47 @@ from aenir._exceptions import InitError, UnitNotFoundError
 
 from dracogate._logging import logger
 
-def bundle_stats(morph):
-    """
-    """
-    stats = []
-    current_stats = morph.current_stats.as_dict()
-    max_stats = morph.max_stats.as_dict()
-    absmax_stats = morph.Stats.ABSOLUTE_MAXES()
-    for indexno, stat in enumerate(morph.Stats.STAT_LIST()):
-        stats.append((stat, current_stats[stat] / 100, max_stats[stat] / 100, absmax_stats[indexno]))
-    return stats
-
-def parse_request_into_args(request):
-    """
-    """
-    game_no = int(request.query_params.get("game_no"))
-    name = request.query_params.get("name")
-    morph_options = (
-        "father",
-        "hard_mode",
-        "number_of_declines",
-        "route",
-        "lyn_mode",
-    )
-    kwargs = {key: request.query_params.get(key) for key in morph_options if request.query_params.get(key) is not None}
-    for kwarg, kwval in kwargs.items():
-        if kwarg in ("hard_mode", "lyn_mode"):
-            kwargs[kwarg] = {
-                "true": True,
-                "false": False,
-            }[kwval]
-        if kwarg == "number_of_declines":
-            kwargs[kwarg] = int(kwval)
-    return (game_no, name, kwargs)
-
 class MorphViewSet(viewsets.ViewSet):
     """
     Handles creation and editing of Morph objects.
     """
     morphs = {}
+
+    @staticmethod
+    def bundle_stats(morph):
+        """
+        """
+        stats = []
+        current_stats = morph.current_stats.as_dict()
+        max_stats = morph.max_stats.as_dict()
+        absmax_stats = morph.Stats.ABSOLUTE_MAXES()
+        for indexno, stat in enumerate(morph.Stats.STAT_LIST()):
+            stats.append((stat, current_stats[stat] / 100, max_stats[stat] / 100, absmax_stats[indexno]))
+        return stats
+
+    @staticmethod
+    def parse_args(dictlike):
+        """
+        """
+        game_no = int(dictlike.get("game_no"))
+        name = dictlike.get("name")
+        morph_options = (
+            "father",
+            "hard_mode",
+            "number_of_declines",
+            "route",
+            "lyn_mode",
+        )
+        kwargs = {key: dictlike.get(key) for key in morph_options if dictlike.get(key) is not None}
+        for kwarg, kwval in kwargs.items():
+            if kwarg in ("hard_mode", "lyn_mode"):
+                kwargs[kwarg] = {
+                    "true": True,
+                    "false": False,
+                }[kwval]
+            if kwarg == "number_of_declines":
+                kwargs[kwarg] = int(kwval)
+        return (game_no, name, kwargs)
 
     def create(self, request):
         """
@@ -59,14 +61,14 @@ class MorphViewSet(viewsets.ViewSet):
             raise Exception
         elif len(morphs) > 5:
             raise Exception
-        morph_id = request.query_params.get("morph_id")
-        if morph_id in morphs:
+        morph_id = request.data.get("morph_id")
+        if morph_id and morph_id in morphs:
             raise Exception
-        game_no, name, kwargs = parse_request_into_args(request)
+        game_no, name, kwargs = self.parse_args(request.data)
         morph = get_morph(game_no, name, **kwargs)
         morph._set_max_level()
         # name, current, max, absMax
-        stats = bundle_stats(morph)
+        stats = self.bundle_stats(morph)
         data = {
             "unitClass": morph.current_cls,
             "level": (morph.current_lv, morph.max_level),
@@ -78,13 +80,13 @@ class MorphViewSet(viewsets.ViewSet):
         """
         Creates temporary Morph for the user to preview, returning missing parameters as necessary.
         """
-        game_no, name, kwargs = parse_request_into_args(request)
+        game_no, name, kwargs = self.parse_args(request.query_params)
         logger.debug("game_no: %d, name: '%s', kwargs: %r", game_no, name, kwargs)
         try:
             morph = get_morph(game_no, name, **kwargs)
             morph._set_max_level()
             # name, current, max, absMax
-            stats = bundle_stats(morph)
+            stats = self.bundle_stats(morph)
             data = {
                 "unitClass": morph.current_cls,
                 "level": (morph.current_lv, morph.max_level),
@@ -98,3 +100,7 @@ class MorphViewSet(viewsets.ViewSet):
             data = {"error": "UNIT_DNE"}
         return Response(data)
 
+    # retrieve
+    # update
+    # partial_update
+    # destroy
