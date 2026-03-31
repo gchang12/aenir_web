9eb9cd917ea5c8a00056b9de45c818524db167bb
gchang12 <gc8@hawaii.edu>
2026-03-31 08:07:12 -1000

    I got my first route up and running.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Hello world</p>,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
