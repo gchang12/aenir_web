f019e4c552181eda20f0aa63ade2322d7b0ff982
gchang12 <gc8@hawaii.edu>
2026-04-01 16:56:15 -1000

    I made a new User model and hooked it up to the project.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+"""
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    UserManager,
+)
+
+class User(AbstractBaseUser):
+    """
+    """
+    email = models.EmailField(
+        unique=True,
+    )
+    username = models.CharField(
+        max_length=26,
+        unique=True,
+    )
+    first_name = None
+    last_name = None
+    is_staff = models.BooleanField(
+        default=False,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+    is_superuser = models.BooleanField(
+        default=False,
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
