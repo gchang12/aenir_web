24771839053173ce82de3e1547224c8b6122c0cb
gchang12 <gc8@hawaii.edu>
2026-04-01 16:10:28 -1000

    I plugged the 'accounts/' module into the project.

--- backend/aenir_web/settings.py
+++ backend/aenir_web/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # MY STUFF
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
