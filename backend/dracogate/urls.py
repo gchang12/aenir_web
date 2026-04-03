5acd536a3155fc2d9236161097d6a03b3d07a770
gchang12 <gc8@hawaii.edu>
2026-04-03 08:38:57 -1000

    I capitalized the 'fe' in the unit-select URL and removed the h3 tag from the unit-select list-items.

--- backend/dracogate/urls.py
+++ backend/dracogate/urls.py
@@ -12,6 +12,6 @@ from . import views
 app_name = "dracogate"
 urlpatterns = [
     path("create_morph/", views.GameSelectView.as_view(), name="game_select"),
-    path("create_morph/fe<int:game_no>/", views.UnitSelectView.as_view(), name="unit_select"),
-    path("create_morph/fe<int:game_no>/<str:name>/", views.UnitConfirmView.as_view(), name="unit_confirm"),
+    path("create_morph/FE<int:game_no>/", views.UnitSelectView.as_view(), name="unit_select"),
+    path("create_morph/FE<int:game_no>/<str:name>/", views.UnitConfirmView.as_view(), name="unit_confirm"),
 ]
