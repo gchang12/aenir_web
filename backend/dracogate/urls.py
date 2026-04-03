131b0ba47548f21f3073a0155c5c14965171acca
gchang12 <gc8@hawaii.edu>
2026-04-03 07:41:37 -1000

    I got the unit-select view up and running.

--- backend/dracogate/urls.py
+++ backend/dracogate/urls.py
@@ -12,4 +12,5 @@ from . import views
 app_name = "dracogate"
 urlpatterns = [
     path("create_morph/", views.GameSelectView.as_view(), name="game_select"),
+    path("create_morph/fe<int:game_no>/", views.UnitSelectView.as_view(), name="unit_select"),
 ]
--- backend/dracogate/views.py
+++ backend/dracogate/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 from aenir.games import FireEmblemGame
+from aenir import morph
 
 class GameSelectView(TemplateView):
     """
@@ -17,4 +18,20 @@ class GameSelectView(TemplateView):
         context = super().get_context_data(**kwargs)
         games = {game.value: game.formal_name for game in FireEmblemGame}
         context['games'] = games
+        return context
+
+class UnitSelectView(GameSelectView):
+    """
+    """
+    template_name = "dracogate/unit_select.html"
+
+    def get_context_data(self, game_no, **kwargs):
+        """
+        """
+        #print(dir(self))
+        #print(kwargs)
+        context = super().get_context_data(**kwargs)
+        morph_cls = getattr(morph, "Morph%d" % game_no)
+        units = morph_cls.CHARACTER_LIST()
+        context['units'] = units
         return context
