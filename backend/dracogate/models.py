65bfa3a841f42fc949ae7de40eda9a034fd38a44
gchang12 <gc8@hawaii.edu>
2026-04-03 14:17:58 -1000

    I declared the fields of the VirtualMorph model.

--- backend/dracogate/models.py
+++ backend/dracogate/models.py
@@ -5,6 +5,8 @@ import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
 class StatBundle:
     """
     """
@@ -17,3 +19,41 @@ class StatBundle:
         self.growth_rate = growth_rate
         self.max_val = max_val / 100
         self.absmax_val = absmax_val / 100
+
+class VirtualMorph(models.Model):
+    """
+    """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    creation_date = models.DateField(
+        auto_now_add=True,
+    )
+    morph_id = models.CharField(
+        max_length=25,
+    )
+    game_no = models.PositiveSmallIntegerField()
+    name = models.CharField(
+        max_length=9,
+    )
+    options = models.JSONField(
+        default=dict,
+        blank=True,
+    )
+    history = models.JSONField(
+        default=list,
+        blank=True,
+    )
+
+    class Meta:
+        """
+        """
+        unique_together = [
+            ["owner", "morph_id"],
+        ]
