d2097ce4b63680c312f14fc83e792e5d6dc74d8f
gchang12 <gc8@hawaii.edu>
2026-04-03 14:15:26 -1000

    I defined the StatBundle class for serialization purposes.

--- backend/dracogate/models.py
+++ backend/dracogate/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+"""
+"""
 
-# Create your models here.
+import uuid
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class StatBundle:
+    """
+    """
+
+    def __init__(self, id: str, current_val: int, growth_rate: int | None, max_val: int, absmax_val: int):
+        """
+        """
+        self.id = id
+        self.current_val = current_val / 100
+        self.growth_rate = growth_rate
+        self.max_val = max_val / 100
+        self.absmax_val = absmax_val / 100
