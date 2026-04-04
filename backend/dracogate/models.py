1b66c0aef73f77f9b62d8037b0c30198d4e53f01
gchang12 <gc8@hawaii.edu>
2026-04-03 20:17:15 -1000

    I erased the StatBundle class.

--- backend/dracogate/models.py
+++ backend/dracogate/models.py
@@ -7,19 +7,6 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class StatBundle:
-    """
-    """
-
-    def __init__(self, id: str, current_val: int, growth_rate: int | None, max_val: int, absmax_val: int):
-        """
-        """
-        self.id = id
-        self.current_val = current_val / 100
-        self.growth_rate = growth_rate
-        self.max_val = max_val / 100
-        self.absmax_val = absmax_val / 100
-
 class VirtualMorph(models.Model):
     """
     """
