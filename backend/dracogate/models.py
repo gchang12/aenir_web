1b12b1df51fee727b9ef1136aef3463fdf3f2b28
gchang12 <gc8@hawaii.edu>
2026-04-04 23:45:19 -1000

    I made a generate_stats method.

--- backend/dracogate/models.py
+++ backend/dracogate/models.py
@@ -7,6 +7,32 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Stat:
+    """
+    """
+
+    def __init__(self, *, id, current_val, growth_rate, max_val, absmax_val, css_class):
+        """
+        """
+        self.id = id
+        self.current_val = current_val
+        self.growth_rate = growth_rate
+        self.max_val = max_val
+        self.absmax_val = absmax_val
+        self.css_class = css_class
+
+    def as_dict(self):
+        """
+        """
+        return {
+            "id": self.id,
+            "current_val": self.current_val,
+            "growth_rate": self.growth_rate,
+            "max_val": self.max_val,
+            "absmax_val": self.absmax_val,
+            "css_class": self.css_class,
+        }
+
 class VirtualMorph(models.Model):
     """
     """
@@ -88,3 +114,23 @@ class VirtualMorph(models.Model):
     def unequip_demi_band(self):
         """
         """
+
+    def _generate_stats(self, css_class_dict=None):
+        """
+        """
+        morph = self.morph
+        current_stats = morph.current_stats.as_dict()
+        growth_rates = morph.growth_rates.as_dict()
+        max_stats = morph.max_stats.as_dict()
+        absmax_stats = morph.Stats.ABSOLUTE_MAXES()
+        # null zero-growth stats
+        zero_growth_stat_list = morph.Stats.ZERO_GROWTH_STAT_LIST()
+        for indexno, statname in enumerate(morph.Stats.STAT_LIST()):
+            yield Stat(
+                id=statname,
+                current_val=current_stats[statname] / 100,
+                growth_rate=(None if statname in zero_growth_stat_list else growth_rates[statname]),
+                max_val=max_stats[statname] / 100,
+                absmax_val=absmax_stats[indexno] / 100,
+                css_class=(None if css_class_dict is None else css_class_dict[statname]),
+            ).as_dict()
--- backend/dracogate/tests.py
+++ backend/dracogate/tests.py
@@ -1,3 +1,100 @@
+"""
+"""
+
 from django.test import TestCase
 
-# Create your tests here.
+from aenir import get_morph
+
+from .models import (
+    VirtualMorph,
+    Stat,
+)
+
+class VirtualMorphTests(TestCase):
+    """
+    """
+
+    def test_generate_stats(self):
+        """
+        """
+        game_no = 6
+        name = "Roy"
+        morph = get_morph(game_no, name)
+        vmorph = VirtualMorph(game_no=game_no, name=name)
+        vmorph.morph = morph
+        expected = (
+            {
+                "id": "HP",
+                "current_val": 18.0,
+                "growth_rate": 80,
+                "max_val": 60.0,
+                "absmax_val": 80.0,
+                "css_class": None,
+            },
+            {
+                "id": "Pow",
+                "current_val": 5.0,
+                "growth_rate": 40,
+                "max_val": 20.0,
+                "absmax_val": 30.0,
+                "css_class": None,
+            },
+            {
+                "id": "Skl",
+                "current_val": 5.0,
+                "growth_rate": 50,
+                "max_val": 20.0,
+                "absmax_val": 30.0,
+                "css_class": None,
+            },
+            {
+                "id": "Spd",
+                "current_val": 7.0,
+                "growth_rate": 40,
+                "max_val": 20.0,
+                "absmax_val": 30.0,
+                "css_class": None,
+            },
+            {
+                "id": "Lck",
+                "current_val": 7.0,
+                "growth_rate": 60,
+                "max_val": 30.0,
+                "absmax_val": 30.0,
+                "css_class": None,
+            },
+            {
+                "id": "Def",
+                "current_val": 5.0,
+                "growth_rate": 25,
+                "max_val": 20.0,
+                "absmax_val": 30.0,
+                "css_class": None,
+            },
+            {
+                "id": "Res",
+                "current_val": 0.0,
+                "growth_rate": 30,
+                "max_val": 20.0,
+                "absmax_val": 30.0,
+                "css_class": None,
+            },
+            {
+                "id": "Con",
+                "current_val": 6.0,
+                "growth_rate": None,
+                "max_val": 20.0,
+                "absmax_val": 25.0,
+                "css_class": None,
+            },
+            {
+                "id": "Mov",
+                "current_val": 5.0,
+                "growth_rate": None,
+                "max_val": 15.0,
+                "absmax_val": 15.0,
+                "css_class": None,
+            },
+        )
+        actual = tuple(vmorph._generate_stats())
+        self.assertTupleEqual(actual, expected)
