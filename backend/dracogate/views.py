31af3e58e97fe82f990488c4fb3f7e1a7acdf88e
gchang12 <gc8@hawaii.edu>
2026-04-07 17:17:33 -1000

    I namespaced the route-params in the templates.

--- backend/dracogate/views.py
+++ backend/dracogate/views.py
@@ -38,11 +38,12 @@ class UnitSelectView(GameSelectView):
         context = super().get_context_data(**kwargs)
         morph_cls = getattr(aenir.morph, "Morph%d" % game_no)
         units = morph_cls.get_true_character_list()
-        context['game_no'] = game_no
+        context['route_params'] = {}
+        context['route_params']['game_no'] = game_no
         context['units'] = units
         return context
 
-class UnitConfirmView(UnitSelectView):
+class UnitConfirmView(UnitSelectView, FormView):
     """
     """
     template_name = "dracogate/unit_confirm.html"
@@ -51,7 +52,56 @@ class UnitConfirmView(UnitSelectView):
         """
         """
         context = super().get_context_data(game_no, **kwargs)
-        context['name'] = name
+        # TODO: namespace names by prefixing them with 'route_params' as appropriate
+        context['route_params']['name'] = name
+        try:
+            morph = get_morph(game_no, name)
+        except InitError as err:
+            default_options = {field: values[0] for field, values in err.init_params.items()}
+            morph = get_morph(game_no, name, **default_options)
+            # TODO: Show form class
+        vmorph = VirtualMorph(game_no=game_no, name=name)
+        vmorph.morph = morph
+        context['active_stats'] = {
+            "current_cls": morph.current_cls,
+            "current_lv": morph.current_lv,
+            "numeric_stats": vmorph._generate_stats(),
+        }
+        return context
+
+    def get_form_class(self):
+        """
+        """
+        print("super().get_form_class())", super().get_form_class())
+        print("get_form_class: self.request.path", self.request.path.split('/'))
+        path = self.request.path.split('/')
+        game_no = int(path[-3].replace("FE", ""))
+        name = path[-2]
+        try:
+            get_morph(game_no, name)
+            init_params = {}
+        except InitError as e:
+            init_params = e.init_params
+        form_class = InitFormBuilder.build_form_class(game_no, name, init_params)
+        # TODO: Check to see if route-params are available at this stage.
+        # Yep.
+        return form_class 
+
+class PreviewMorphView(TemplateView, FormView):
+    """
+    """
+    template_name = "dracogate/_preview_morph.html"
+    form_class = None
+
+    def get_form_class(self):
+        """
+        """
+        # TODO: Check to see if route-params are available at this stage.
+
+    # TODO: Need to do research on htmx to progress.
+    def get_context_data(self, game_no, name, **kwargs):
+        """
+        """
         try:
             morph = get_morph(game_no, name)
         except InitError as err:
