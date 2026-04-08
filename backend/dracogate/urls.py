67dafb45bcdabe483589e52a296334cdf4b32b8f
gchang12 <gc8@hawaii.edu>
2026-04-07 17:19:11 -1000

    I inserted a colon into the title.

--- backend/dracogate/urls.py
+++ backend/dracogate/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path("create_morph/", views.GameSelectView.as_view(), name="game_select"),
     path("create_morph/FE<int:game_no>/", views.UnitSelectView.as_view(), name="unit_select"),
     path("create_morph/FE<int:game_no>/<str:name>/", views.UnitConfirmView.as_view(), name="unit_confirm"),
+    path("_preview_morph/FE<int:game_no>/<str:name>/", views.PreviewMorphView.as_view(), name="_preview_morph"),
 ]
