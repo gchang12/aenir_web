834e40eead8283a7317fac8ddc4c2fb4a335ff01
gchang12 <gc8@hawaii.edu>
2026-04-09 22:31:29 -1000

    I implemented the ModifyMorphView some more.

--- backend/dracogate/urls.py
+++ backend/dracogate/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path("create_morph/FE<int:game_no>/<str:name>/", views.UnitConfirmView.as_view(), name="unit_confirm"),
     path("_preview_morph/FE<int:game_no>/<str:name>/", views.PreviewMorphView.as_view(), name="_preview_morph"),
     path("modify_morphs/", views.ListMorphsView.as_view(), name="list_morphs"),
-    path("modify_morphs/<uuid:id>/", views.ModifyMorphView.as_view(), name="modify_morph"),
+    path("modify_morphs/<uuid:pk>/", views.ModifyMorphView.as_view(), name="modify_morph"),
 ]
