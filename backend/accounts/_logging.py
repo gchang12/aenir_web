85c4739ce0dd276b9c433d57819561c97c711b4c
gchang12 <gc8@hawaii.edu>
2026-05-26 23:03:34 -1000

    I deleted the account application.

--- backend/accounts/_logging.py
+++
@@ -1,6 +0,0 @@
-"""
-"""
-
-import logging
-
-logger = logging.getLogger("aenir_web.accounts")
--- backend/aenir_web/urls.py
+++ backend/aenir_web/urls.py
@@ -23,7 +23,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("accounts.urls")),
     path("dracogate/", include("dracogate.urls")),
     path("", TemplateView.as_view(template_name="root.html")),
 ]
