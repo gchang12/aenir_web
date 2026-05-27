fe15b70d8ec77f0906add53054cf532289535147
gchang12 <gc8@hawaii.edu>
2026-05-27 12:17:30 -1000

    I took out the URL prefix for dracogate.

--- backend/aenir_web/urls.py
+++ backend/aenir_web/urls.py
@@ -23,6 +23,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("dracogate/", include("dracogate.urls")),
+    path("", include("dracogate.urls")),
     path("", TemplateView.as_view(template_name="_root.html")),
 ]
