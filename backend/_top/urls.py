33842451036f6bb97d8fb218f50e398afc8d5c39
gchang12 <gc8@hawaii.edu>
2026-09-17 08:43:58 -1000

    Added _top module.

---
+++ backend/_top/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+#from django.http import HttpResponse
+
+'''
+def index(request):
+    return HttpResponse("Hello world")
+'''
+
+urlpatterns = [
+    #path("", index, name="index"),
+]
