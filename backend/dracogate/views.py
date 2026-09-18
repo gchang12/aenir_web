53410404a627cff15e826244ee37b0cf94611691
gchang12 <gc8@hawaii.edu>
2026-09-18 08:16:40 -1000

    morph_init view is up and operational

--- backend/dracogate/views.py
+++ backend/dracogate/views.py
@@ -3,9 +3,25 @@
 
 from django.views.generic import base, edit
 
+from aenir.games import FireEmblemGame
+
 class CreateMorphView(base.TemplateView):
     """
     """
+    template_name = "dracogate/morph_init.html"
+
+    def get_context_data(self, **kwds):
+        """
+        """
+        print(kwds)
+        context = super().get_context_data(**kwds)
+        context['games'] = [
+            {
+                "title": fe_game.formal_name,
+                "game_no": fe_game.value,
+            } for fe_game in FireEmblemGame
+        ]
+        return context
 
 class GameSelectView(base.TemplateView):
     """
@@ -17,6 +33,6 @@ class UnitSelectView(edit.CreateView):
     """
     """
 
-class UnitSelectForm(base.FormView):
+class UnitSelectForm(edit.FormView):
     """
     """
