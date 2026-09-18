0939a0c52d9355bb7f83aaa726dbac8479c706d2
gchang12 <gc8@hawaii.edu>
2026-09-18 07:56:13 -1000

    Initialized views for morph-init

--- backend/dracogate/urls.py
+++ backend/dracogate/urls.py
@@ -5,5 +5,10 @@ from django.urls import path, include
 
 from . import views
 
+app_name = "dracogate"
 urlpatterns = [
+    path("create/", views.CreateMorphView.as_view(), name="morph_init"),
+    path("create/FE<int:game_no>/", views.GameSelectView.as_view(), name="game_select"),
+    path("create/FE<int:game_no>/<str:unit>/", views.UnitSelectView.as_view(), name="unit_select"),
+    path("forms/create/FE<int:game_no>/<str:unit>/", views.UnitSelectForm.as_view(), name="unit_select_form"),
 ]
--- backend/dracogate/views.py
+++ backend/dracogate/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""
+"""
 
-# Create your views here.
+from django.views.generic import base, edit
+
+class CreateMorphView(base.TemplateView):
+    """
+    """
+
+class GameSelectView(base.TemplateView):
+    """
+    """
+
+# TODO: Figure out how to implement these
+
+class UnitSelectView(edit.CreateView):
+    """
+    """
+
+class UnitSelectForm(base.FormView):
+    """
+    """
