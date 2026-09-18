184032eec7c0b26b714a73823c818b282aaf7c47
gchang12 <gc8@hawaii.edu>
2026-09-17 20:27:53 -1000

    Initialized and plugged in module: 'dracogate'

--- backend/aenir_web/settings.py
+++ backend/aenir_web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "_top.apps.TopConfig",
+    "dracogate.apps.DracogateConfig",
     "django_browser_reload",
 ]
 
