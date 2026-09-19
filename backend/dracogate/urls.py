651b239f10891b798f1ba85aab1f9a3e0d9c9208
gchang12 <gc8@hawaii.edu>
2026-09-18 15:55:55 -1000

    More work for 'unit_confirm'

--- backend/dracogate/urls.py
+++ backend/dracogate/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("create/", views.GameSelectView.as_view(), name="game_select"),
     path("create/FE<int:game_no>/", views.UnitSelectView.as_view(), name="unit_select"),
     path("create/FE<int:game_no>/<str:unit>/", views.UnitConfirmView.as_view(), name="unit_confirm"),
-    path("forms/create/FE<int:game_no>/<str:unit>/", views.UnitConfirmForm.as_view(), name="unit_confirm_form"),
+    path("components/create/FE<int:game_no>/<str:unit>/", views.UnitConfirmForecast.as_view(), name="unit_confirm_forecast"),
 ]
