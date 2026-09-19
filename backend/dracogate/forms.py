afa7c6653b78d8f1ce2058ef7f72c108c1e3fe5a
gchang12 <gc8@hawaii.edu>
2026-09-18 15:54:21 -1000

    Declared new init-form class builder

---
+++ backend/dracogate/forms.py
@@ -0,0 +1,91 @@
+"""
+"""
+
+from django import forms
+
+class InitFormBuilder:
+    """
+    """
+
+    @staticmethod
+    def father(choices):
+        """
+        """
+        return forms.ChoiceField(
+            choices=[(choice, choice) for choice in choices],
+            required=True,
+            initial=choices[0],
+            label="Father",
+        )
+
+    @staticmethod
+    def hard_mode(choices):
+        """
+        """
+        return forms.BooleanField(
+            required=True,
+            initial=choices[0],
+            label="Hard Mode",
+        )
+
+    @staticmethod
+    def number_of_declines(choices):
+        """
+        """
+        return forms.IntegerField(
+            min_value=choices[0],
+            max_value=choices[-1],
+            step_size=1,
+            required=True,
+            initial=choices[0],
+            label="Number of Declines",
+        )
+
+    @staticmethod
+    def chapter(choices):
+        """
+        """
+        return forms.ChoiceField(
+            choices=[(choice, choice) for choice in choices],
+            required=True,
+            initial=choices[0],
+            label="Chapter",
+        )
+
+    @staticmethod
+    def lyn_mode(choices):
+        """
+        """
+        return forms.BooleanField(
+            required=True,
+            initial=choices[0],
+            label="Lyn Mode",
+        )
+
+    @classmethod
+    def build_form_class(cls, init_params):
+        """
+        """
+        class InitForm(forms.Form):
+            """
+            """
+            game_no = forms.IntegerField(
+                min_value=4,
+                max_value=9,
+                step_size=1,
+            )
+            name = forms.CharField(
+                max_length=9,
+            )
+        for init_param, choices in init_params.items():
+            InitForm.declared_fields[init_param] = getattr(cls, init_param)(choices)
+        return InitForm
+
+'''
+    def __init__(self, name: str, *, father: str | None = None):
+    def __init__(self, name: str):
+    def __init__(self, name: str, *, hard_mode: bool | None = None, number_of_declines: int | None = None, chapter: str | None = None):
+    def __init__(self, name: str, *, lyn_mode: bool | None = None, hard_mode: bool | None = None):
+    def __init__(self, name: str):
+    def __init__(self, name: str):
+'''
--- backend/dracogate/tests.py
+++ backend/dracogate/tests.py
@@ -1,3 +1,89 @@
-from django.test import TestCase
+"""
+"""
 
-# Create your tests here.
+from django.test import TestCase
+import django.forms
+
+from aenir import (
+    get_morph,
+    InitError,
+)
+
+from . import forms
+
+class InitFormBuilderTests(TestCase):
+    """
+    """
+
+    def test_father(self):
+        """
+        """
+        try:
+            get_morph(4, "Lakche")
+            init_params = {}
+        except InitError as e:
+            init_params = e.init_params
+        form_class = forms.InitFormBuilder.build_form_class(init_params)
+        self.assertIn("father", form_class.declared_fields)
+        field = form_class.declared_fields["father"]
+        self.assertIsInstance(field, django.forms.ChoiceField)
+        choices = init_params.pop("father")
+        self.assertEqual(field.choices, [(choice, choice) for choice in choices])
+
+    def test_hard_mode(self):
+        """
+        """
+        try:
+            get_morph(6, "Rutger")
+            init_params = {}
+        except InitError as e:
+            init_params = e.init_params
+        form_class = forms.InitFormBuilder.build_form_class(init_params)
+        self.assertIn("hard_mode", form_class.declared_fields)
+        field = form_class.declared_fields["hard_mode"]
+        self.assertIsInstance(field, django.forms.BooleanField)
+
+    def test_number_of_declines(self):
+        """
+        """
+        try:
+            get_morph(6, "Hugh")
+            init_params = {}
+        except InitError as e:
+            init_params = e.init_params
+        form_class = forms.InitFormBuilder.build_form_class(init_params)
+        self.assertIn("number_of_declines", form_class.declared_fields)
+        field = form_class.declared_fields["number_of_declines"]
+        self.assertIsInstance(field, django.forms.IntegerField)
+        self.assertEqual(field.min_value, 0)
+        self.assertEqual(field.max_value, 3)
+
+    def test_chapter(self):
+        """
+        """
+        try:
+            get_morph(6, "Gonzales")
+            init_params = {}
+        except InitError as e:
+            init_params = e.init_params
+        form_class = forms.InitFormBuilder.build_form_class(init_params)
+        self.assertIn("hard_mode", form_class.declared_fields)
+        self.assertIn("chapter", form_class.declared_fields)
+        field = form_class.declared_fields["chapter"]
+        self.assertIsInstance(field, django.forms.ChoiceField)
+        choices = init_params.pop("chapter")
+        self.assertEqual(field.choices, [(choice, choice) for choice in choices])
+
+    def test_lyn_mode(self):
+        """
+        """
+        try:
+            get_morph(7, "Lyn")
+            init_params = {}
+        except InitError as e:
+            init_params = e.init_params
+        form_class = forms.InitFormBuilder.build_form_class(init_params)
+        self.assertIn("lyn_mode", form_class.declared_fields)
+        field = form_class.declared_fields["lyn_mode"]
+        self.assertIsInstance(field, django.forms.BooleanField)
+
